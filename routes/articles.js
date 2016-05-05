a84c8b3b46599b7daebe11d10efc42893c4828df
vvargin <v.vargin@team.amocrm.com>
2016-05-05 20:06:31 +0300

    Initial commit

---
+++ routes/articles.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET articles listing. */
+router.get('/', function (req, res, next) {
+  res.render('articles/list', {title: 'Express'});
+});
+
+router.get('/add', function (req, res, next) {
+  res.render('articles/add', {title: 'Express'});
+});
+
+router.post('/create', function (req, res, next) {
+  res.redirect('/articles/add');
+});
+
+module.exports = router;
