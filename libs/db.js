4bb56a09bb71be991443d05b94f0050d72950289
vvargin <v.vargin@team.amocrm.com>
2016-05-07 22:25:08 +0300

    make singleton better

--- libs/db.js
+++ libs/db.js
@@ -2,8 +2,10 @@ var
   monk = require('monk'),
   dbInstance;
 
-if (!dbInstance) {
-  dbInstance = monk('localhost:27017/node_blog');
-}
+module.exports = function () {
+  if (!dbInstance) {
+    dbInstance = monk('localhost:27017/node_blog');
+  }
 
-module.exports = dbInstance;
+  return dbInstance;
+};
