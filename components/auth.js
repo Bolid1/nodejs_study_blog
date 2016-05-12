ab83a7d6329910938d8b63a1eabe687e17af6688
vvargin <v.vargin@team.amocrm.com>
2016-05-12 22:06:48 +0300

    Add user to locales

--- components/auth.js
+++ components/auth.js
@@ -2,7 +2,8 @@ var
   Component,
   sessions = require('express-session'),
   /** @var Authenticator passport */
-  passport = require('../libs/passport')();
+  passport = require('../libs/passport')(),
+  Users = require('./../libs/users');
 
 Component = function () {
 };
@@ -18,6 +19,30 @@ Component.fillApp = function (app) {
   // session.
   app.use(passport.initialize({}));
   app.use(passport.session({}));
+
+  app.use(function (req, res, next) {
+    if (!(req.user && req.user._id)) {
+      next();
+      return;
+    }
+
+    var user = new Users.Model({
+      _id: req.user._id
+    });
+
+    user.fetch({
+      success: function () {
+        if (!user.isNew()) {
+          res.locals.user = user.toJSON();
+        }
+
+        next();
+      },
+      error: function () {
+        next();
+      }
+    });
+  });
 };
 
 module.exports = Component;
